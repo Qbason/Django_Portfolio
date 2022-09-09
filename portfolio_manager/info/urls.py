41ed5d038fffa2694cdeab0bc9292f7b15cc0277
Jakub K <kuba1095@gmail.com>
2022-09-09 13:23:31 +0200

    cache + robots.txt

--- portfolio_manager/info/urls.py
+++ portfolio_manager/info/urls.py
@@ -1,8 +1,7 @@
 from info import views
-from info.views_webpage import my_view_lang,my_view_lang_en,my_view_lang_pl
+from info.views_webpage import my_view_lang,my_view_lang_en,my_view_lang_pl, robots_txt
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
 
 router = DefaultRouter()
 
@@ -15,5 +14,6 @@ urlpatterns = [
     path("",my_view_lang_en,name='index'),
     path("pl",my_view_lang_pl,name='lang_pl'),
     path("en",my_view_lang_en,name='lang_en'),
-    path("api/",include(router.urls))
+    path("api/",include(router.urls)),
+    path("robots.txt",robots_txt)
 ]
--- portfolio_manager/info/views_webpage.py
+++ portfolio_manager/info/views_webpage.py
@@ -5,6 +5,19 @@ from info.models import ContentWebPage
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
+from django.views.decorators.http import require_GET
+from django.http import HttpResponse
+
+@require_GET
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Allow: /"
+
+    ]
+    return HttpResponse("\n".join(lines),content_type="text/plain")
+
+
 def page_not_found_view(request, exception):
     return render(request,'404.html',status=404)
 
